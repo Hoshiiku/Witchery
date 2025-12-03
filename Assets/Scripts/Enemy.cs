2716d9aa18f507f9e6868a43bfa25d3d94861bbf
Hoshiiku <camilito2908mail.com>
2025-12-02 22:00:49 -0500

    Update
    Added a small cinematic. Fixed animations. Added eneemy blueprint

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,79 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] protected float speed = 3f;
+    [SerializeField] protected float detectionRange = 10f;
+    [SerializeField] protected float health = 50f;
+    [SerializeField] protected float damage = 10f;
+    protected Rigidbody2D rb;
+    protected Animator anim;
+    protected SpriteRenderer sprite;
+    bool dead = false;
+    protected GameObject player;
+    protected float attackCooldown = 1.5f;
+    protected float distance;
+
+
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindObjectOfType<PlayerControl>().gameObject;
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        if (!dead)
+        {
+            Attack();
+        }
+        
+    }
+    private void FixedUpdate()
+    {
+        if (!dead)
+        {
+            Move();
+        }
+    }
+
+
+
+    public virtual void Move()
+    {
+        
+    }
+    public virtual void Attack()
+    {
+        
+    }
+
+
+
+
+    public void Changehealth(float count)
+    {
+        health -= count;
+        if (health <= 0)
+        {
+            dead = true;
+
+            GetComponent<Collider2D>().enabled = false;
+            anim.SetBool("Die", true);
+            rb.linearVelocity = Vector2.zero;
+
+
+        }
+    }
+}
